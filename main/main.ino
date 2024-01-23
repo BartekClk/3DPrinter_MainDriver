7f790efed7e4333b00312d92b24379da85278b7e
Bartek <clkbartek@gmail.com>
2024-01-23 19:17:17 +0100

    modified:   main/main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,45 +1,95 @@
 #include <Arduino.h>
+#include <stdint.h>
 
-class Pin{
+class Loop{
+  private:
+    static unsigned long actualTime;
+  protected:
+    unsigned long deltaTime = 0;
+    unsigned long lastTime = 0;
+
+  public:
+    Loop(){
+      lastTime = actualTime;
+    }
+
+
+    static unsigned long getActualTime(){return actualTime;}
+    static void setActualTime(unsigned long time){actualTime = time;}
+};
+unsigned long Loop::actualTime = 0;
+
+class Pin{ //Class for defining pins
   protected:
     byte pin;
-    String mode;
+    int mode; // 0 = INPUT, 1 = OUTPUT, 2 = INPUT_PULLUP
+    byte defState;
   public:
-    Pin(byte pin, String mode){
+    Pin(byte pin, int mode, byte defState=0){
       this->pin = pin;
       this->mode = mode;
+      this->defState = defState;
     }
+
     byte getPin(){return pin;}
+
+    void defPin(){
+      switch (mode)
+      {
+      case 0:
+        pinMode(pin, INPUT);
+        break;
+      case 1:
+        pinMode(pin, OUTPUT);
+        break;
+      case 2:
+        pinMode(pin, INPUT_PULLUP);
+        break;
+      }
+      if(defState == 1) digitalWrite(pin, HIGH);
+      else digitalWrite(pin, LOW);
+    }
+
+    int readPin(){
+      return digitalRead(pin);
+    }
+
+    void setPin(byte state){
+      digitalWrite(pin, state);
+    }
 };
 
-class Fan{
+class Fan{ //Class for defining fans
   protected:
     int speed = 0;
     Pin *pin;
 
   public:
-    Fan(byte pin, String pinMode){
-      this->pin = new Pin(pin, pinMode);
+    Fan(byte pin){
+      this->pin = new Pin(pin, 1);
     }
-    byte getPin(){return pin->getPin();}
     void setSpeed(int speed){
       this->speed = speed;
     }
+    void def(){ 
+      pin->defPin();
+    }
 };
 
-class Heater{
+class Heater{ //Class for defining heaters
   protected:
     int actualTemp = 10;
     int wantedTemp = 60;
     bool isHeating = 0;
-    byte Pin;
-    byte defState = 0;
+    Pin *pin;
 
   public:
-    Heater(byte Pin){
-      this->Pin = Pin;
-      pinMode(this->Pin, OUTPUT);
-      digitalWrite(this->Pin, HIGH);
+    Heater(byte pin){
+      this->pin = new Pin(pin, 1);
+    }
+    
+  void def(){
+      pin->defPin();
     }
 
   int getTemp(bool a=0){ // 0 = actualTemp, 1 = wantedTemp;
@@ -54,12 +104,6 @@ class Heater{
   }
 
   bool getIsHeating(){return isHeating;}
-  byte getPin(){return Pin;}
-
-  void def(){
-    // pinMode(Pin, )
-    if(defState == 0);
-  }
 };
 
 class HeatBed : public Heater{
@@ -70,10 +114,6 @@ class HeatBed : public Heater{
 
 class HotEnd : public Heater{
   private:
-    float AxisPosition[3] = {0,0,0}; //x,y,z
-    float AxisPositionHome[3] = {0,0,0}; //x,y,z
-    bool positionTrusted = false;
-    bool levelingAfterHome = true;
   public:
     HotEnd(byte transistorPin) : Heater(transistorPin){}
 };
@@ -82,45 +122,119 @@ class Limiter{
   private:
     String name;
     bool status = 0;
-    byte read_pin;
+    Pin *pin;
   public:
-    Limiter(byte read_pin){
-      this->read_pin = read_pin;
-      name = "unnamed";
-      }
-    
-    Limiter(byte read_pin, String name){
-      this->read_pin = read_pin;
-      this->name = name;
+    Limiter(byte pin, String name = "unnamed"){
+      this->pin = new Pin(pin, 2);
+
+      if (name=="unnamed") this->name = "unnamed";
+      else this->name = name;
     }
 
-    void checkStatus(){
-        switch(digitalRead(read_pin)){
-          case HIGH:
-            status = 1;
-            break;
-          case LOW:
-            status = 0;
-            break;
-        }
+    bool getStatus(){
+        status = pin->readPin();
+        return status;
     }
 
-    bool getStatus(){return status;}
     String getName(){return name;}
-    byte getPin(){return read_pin;}
+};
+
+class Motor{
+  private:
+    Pin *dirPin;
+    Pin *stepPin;
+    Pin *enablePin;
+    float mmPerStep;
+    
+    public:
+      Motor(byte dirPin, byte stepPin, byte enablePin, float mmPerStep){
+        this->dirPin = new Pin(dirPin, 1);
+        this->stepPin = new Pin(stepPin, 1);
+        this->enablePin = new Pin(enablePin, 1);
+        this->mmPerStep = mmPerStep;
+      }
+
+    void def(byte dir = 0, byte enable = 0, byte ms = 0){
+      dirPin->defPin();
+      dirPin->setPin(dir);
+
+      stepPin->defPin();
+
+      enablePin->defPin();
+      enablePin->setPin(enable);
+    }
+};
+
+class MicroSteps{
+  private:
+    Pin *pin[3];
+  public:
+    MicroSteps(byte msPins[3]){
+      pin[0] = new Pin(msPins[0], 1);
+      pin[1] = new Pin(msPins[1], 1);
+      pin[2] = new Pin(msPins[2], 1);
+    }
+
+    void setMs(byte ms=0){ // 0 = full step, 1 = half step, 2 = 1/4 step, 3 = 1/8 step, 4 = 1/16 step
+      switch (ms)
+      {
+      case 0: // full step
+        pin[0]->setPin(0);
+        pin[1]->setPin(0);
+        pin[2]->setPin(0);
+        break;
+      case 1: // half step
+        pin[0]->setPin(1);
+        pin[1]->setPin(0);
+        pin[2]->setPin(0);
+        break;
+      case 2: // 1/4 step
+        pin[0]->setPin(0);
+        pin[1]->setPin(1);
+        pin[2]->setPin(0);
+        break;
+      case 3: // 1/8 step
+        pin[0]->setPin(1);
+        pin[1]->setPin(1);
+        pin[2]->setPin(0);
+        break;
+      case 4: // 1/16 step
+        pin[0]->setPin(1);
+        pin[1]->setPin(1);
+        pin[2]->setPin(1);
+        break;
+      default: // full step
+        pin[0]->setPin(0);
+        pin[1]->setPin(0);
+        pin[2]->setPin(0);
+        break;
+      }
+    }
 };
 
 class Printer{
   private:
     HeatBed *heatBed = new HeatBed(22);
     HotEnd *hotEnd = new HotEnd(23);
-    Fan *hotEndFan = new Fan(2, "OUTPUT");
+
+    Fan *hotEndFan = new Fan(2);
+    Fan *printFan = new Fan(3);
+
+
+
+    Motor *xMotor = new Motor(5,6,7, 0.29215);
+
     int printSpeed = 1;
     bool absoluteExtrusion = true;
     bool imperialUnits = false;
     bool absolutePositioning = true;
+
+    float AxisPosition[3] = {0,0,0}; //x,y,z
+    float AxisPositionHome[3] = {0,0,0}; //x,y,z
+    bool positionTrusted = false;
+    bool levelingAfterHome = true;
   public:
-    Limiter *endStops[3][2] = {
+    Limiter *limiters[3][2] = {
       {}, //x dimmension
       {}, //y dimmension
       {}  //z dimmension
@@ -130,33 +244,52 @@ class Printer{
 
     }
 
-  void setEndStops(byte a[]){
-    endStops[0][0] = new Limiter(a[0], "y-front");
-    endStops[0][1] = new Limiter(a[1], "y-back");
+  void setLimiters(byte a[]){
+    limiters[0][0] = new Limiter(a[0], "y-front");
+    limiters[0][1] = new Limiter(a[1], "y-back");
 
-    endStops[1][0] = new Limiter(a[2], "x-left");
-    endStops[1][1] = new Limiter(a[3], "x-right");
+    limiters[1][0] = new Limiter(a[2], "x-left");
+    limiters[1][1] = new Limiter(a[3], "x-right");
 
-    endStops[2][0] = new Limiter(a[4], "z-top");
-    endStops[2][1] = new Limiter(a[5], "z-bottom");
+    limiters[2][0] = new Limiter(a[4], "z-top");
+    limiters[2][1] = new Limiter(a[5], "z-bottom");
   }
 
   int getBedT(bool a=0){return heatBed->getTemp(a);}
   int getHotEndT(bool a=0){return hotEnd->getTemp(a);}
   int getPrintSpeed(){return printSpeed;}
-  bool getendStopstatus(byte row, byte collumn){return endStops[row][collumn]->getStatus();}
+  bool getendStopstatus(byte row, byte collumn){return limiters[row][collumn]->getStatus();}
+
+  void def(){
+    heatBed->def();
+    hotEnd->def();
+    hotEndFan->def();
+    printFan->def();
+    setLimiters(new byte[6]{26,27,28,29,30,31});
+  }
 };
 
 Printer *printer = new Printer();
 
-HeatBed *a = new HeatBed(22);
+
+unsigned long lastTime = 0;
+unsigned long deltaTime = 0;
 
 void setup() {
-  byte endStopsPins[6] = {26,27,28,29,30,31};
-  printer->setEndStops(endStopsPins);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("Hello World");
+  printer->def();
 }
 
+Loop loop1 = Loop();
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Loop::setActualTime(millis());
+
+  // deltaTime = actualTime - lastTime;
+
+  // if (deltaTime >= 1000UL){
+  //   Serial.println(actualTime);
+  //   lastTime = actualTime;
+  // }
 }
